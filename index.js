c4413463aad3b01ad8c249d7015fa86b9c82bd23
Gideon Macharia <kamaugedion95@gmail.com>
2024-03-31 22:26:19 +0300

    finish eventListener lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//let input = document.getElementById('button')
+//input.addEventListener('click', () =>{
+  //alert("I was clicked")
+//})
+
+const input = document.getElementById('button');
+
 function addingEventListener() {
+  alert('I was clicked!');
+  input.addEventListener('click', addingEventListener);
 }
+
